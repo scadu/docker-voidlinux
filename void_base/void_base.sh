b35dcd3617593ac3ca97b80da3fed797153fbb47
Łukasz Jendrysik <scadu@disroot.org>
2018-03-17 18:46:44 +0100

    remove not needed images, modify build script

--- void_base/void_base.sh
+++ void_base/void_base.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 CHROOTDIR="./base"
+REPO="http://repo2.voidlinux.eu/current"
 
 [[ -d ${CHROOTDIR} ]] && rm -rf ${CHROOTDIR}
 mkdir ${CHROOTDIR}
 mkdir -p ${CHROOTDIR}/var/db/xbps/keys/
 cp -a /var/db/xbps/keys/* ${CHROOTDIR}/var/db/xbps/keys/
 
-xbps-install -yS -r ${CHROOTDIR} -R http://repo.voidlinux.eu/current base-voidstrap
+xbps-install -yS -r ${CHROOTDIR} -R ${REPO} base-voidstrap
 rm -rf ${CHROOTDIR}/var/cache/xbps
 
-[[ -e void_base.tar ]] && rm -f void_base.tar
+[[ -e void_base.tar.xz ]] && rm -f void_base.tar.xz
 cd ${CHROOTDIR}
-tar cf ../void_base.tar .
+tar cfJ ../void_base.tar.xz .
